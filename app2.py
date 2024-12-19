60e2a17e569725fc86493c450a5af608665327c3
Gatla Durgesh Reddy <143740185+durgeshreddygatla@users.noreply.github.com>
2024-12-19 23:11:27 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ import os
 # Initialize Flask app
 app = Flask(__name__)
 
-os.environ["HUGGING_FACE_API_KEY"] = "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"
+os.environ["HUGGING_FACE_API_KEY"] = "" # Use your own token
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
